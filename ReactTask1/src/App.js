dbf0433b36a1d74abf007c1ea533c2cfd52ada4f
Arda <111998578+gokcearda@users.noreply.github.com>
2024-12-19 15:44:09 +0300

    Vercel speed-insight kuruldu

--- ReactTask1/src/App.js
+++ ReactTask1/src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import TaskForm from "./components/TaskForm";
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
     return (
         <div className="app-container">
             <TaskForm />
-            <SpeedInsights />
-
         </div>
     );
 }
